34dfdd54399723b6738ebaf0eae24def681f8c44
JS <jsedrak@student.ie.edu>
2021-03-10 09:57:01 -0500

    Homework re Translator

---
+++ dictionary_defined.py
@@ -0,0 +1,44 @@
+dictionary = {}
+
+engspn = {
+    "Hello":"Hola",
+    "Goodbye": "Adios",
+    "Good Morning": "Buenos dias",
+    "Good Evening": "Buenas tardes",
+    "Good Night": "Buenas noches",
+    "Welcome":  "Bienvenido",
+    "How are you?": "Como Estas?",
+    "How\'s it going?": "Como Te Va?",
+    "How\'ve you been?": "Como te ha ido?",
+    "I\'m fine thank you": "Estoy bien Gracias",
+    "Thank you": "Gracias",
+    "Please": "Por favor",
+    "You\'re welcome": "De nada!",
+    "Excuse me": "Perdone!",
+    "Sorry": "Lo siento!",
+    "Have fun": "Diviertete",
+    "Well done!": "Muy bien!",
+    "Take care!": "Cuidate!",
+    "See you later": "Hasta luego!",
+    "See you soon": "Hasta pronto!",
+    "See you tomorrow": "Hasta manana!",
+    "See you": "Nos vemos",
+    "Cheers": "Salud",
+    "Yes": "Si",}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
