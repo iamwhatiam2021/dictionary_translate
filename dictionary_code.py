3e6d7fc078a01dac9c71049fe2b6691b578c81c7
JS <jsedrak@student.ie.edu>
2021-03-10 09:54:41 -0500

    Initial commit

---
+++ dictionary_code.py
@@ -0,0 +1,22 @@
+import dictionary_defined
+
+print(dictionary_defined.engspn)
+def transalte(engspn):
+    """
+    
+    :param engspn: english: spanish 
+    :return: translated words
+    """
+
+def english(eng):
+    pass
+
+
+def spanish(spn):
+    pass
+
+transalted_words = (english(spanish))
+
+for word in dictionary_defined.engspn:
+    transalted_words.read(dictionary_defined.get(word, "[no translation"))
+    print(f"{english} -> {spanish}")
